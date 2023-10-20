b70df5558ff7b49092818942fb12abd00d7883b7
Nandini Gupta <100197669+Nandini99-git@users.noreply.github.com>
2023-10-20 20:19:31 +0530

    Update and rename class with object.p to class with object.py

--- class with object.py
+++ class with object.py
@@ -1,7 +1,7 @@
 class Dog:
 
 	attr1 = "mammal"
- 
+
 	def __init__(self, name):
 		self.name = name
 
@@ -13,3 +13,4 @@ print("Tommy is also a {}".format(Tommy.__class__.attr1))
 
 print("My name is {}".format(Rodger.name))
 print("My name is {}".format(Tommy.name))
+
